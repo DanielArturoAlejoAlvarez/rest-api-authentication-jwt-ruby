3d91a45e2bd415a0892935312fe4e71438b36e7f
Daniel Arturo Alejo Alvarez <18106359+DanielArturoAlejoAlvarez@users.noreply.github.com>
2020-09-28 10:11:36 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 2020_09_27_151132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "avatar"
-  end
+  
 
 end
